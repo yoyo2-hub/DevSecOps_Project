5569a936bcd21d57a232d527847270052da4dc8c
Herin Patel <herin.p45@gmail.com>
2025-05-12 14:12:09 -0400

    Implement user registration in AuthService

--- src/main/java/com/herin/ecommerce/repository/UserRepository.java
+++ src/main/java/com/herin/ecommerce/repository/UserRepository.java
@@ -8,4 +8,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     Optional<UserEntity> findByEmail(String email);
     boolean existsByEmail(String email);
+
+    Optional<Object> findByUsername(String username);
+    boolean existsByUsername(String username);
 }
---
+++ src/main/java/com/herin/ecommerce/service/AuthService.java
@@ -0,0 +1,36 @@
+package com.herin.ecommerce.service;
+
+import com.herin.ecommerce.dto.UserRequestDTO;
+import com.herin.ecommerce.dto.UserResponseDTO;
+import com.herin.ecommerce.model.UserEntity;
+import com.herin.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserResponseDTO register(UserRequestDTO userRequestDTO) {
+        if (userRepository.existsByUsername(userRequestDTO.getUsername())) {
+            throw new RuntimeException("User already exists");
+        }
+        if (userRepository.existsByEmail(userRequestDTO.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+        UserEntity user = new UserEntity(userRequestDTO.getUsername(),
+                passwordEncoder.encode(userRequestDTO.getPassword()),
+                userRequestDTO.getEmail());
+        userRepository.save(user);
+        return new UserResponseDTO(user.getId(), user.getUsername(), user.getEmail());
+    }
+}
