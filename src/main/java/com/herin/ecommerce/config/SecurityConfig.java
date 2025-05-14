5b4740ba1323891e421844cb05b991119789df35
Herin Patel <herin.p45@gmail.com>
2025-05-14 17:56:23 -0400

    Disable CSRF and configure stateless session management

--- src/main/java/com/herin/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/herin/ecommerce/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,14 +22,13 @@ public class SecurityConfig {
     // Security filter chain bean
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable) // disables CSRF
+        http.csrf(AbstractHttpConfigurer::disable) // disables CSRF
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/**").permitAll()  // allow public access to auth endpoints
                         .anyRequest().authenticated()                    // protect everything else
                 )
-                .httpBasic(Customizer.withDefaults()); // enables basic auth (optional for dev)
-
-        return http.build();
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    return http.build();
     }
 }
