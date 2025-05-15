6d0d7a95f0b52493c03ec42f7a46bca1d17278fa
Herin Patel <herin.p45@gmail.com>
2025-05-15 12:43:38 -0400

    Added AuthenticationProvider in SecurityConfig.java

--- src/main/java/com/herin/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/herin/ecommerce/config/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.herin.ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,6 +16,9 @@ import org.springframework.security.config.Customizer;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     // Password encoder bean
     @Bean
@@ -24,11 +31,20 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable) // disables CSRF
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll()  // allow public access to auth endpoints
+                        .requestMatchers("/api/auth/**").permitAll()  // allow public access to auth endpoints
                         .anyRequest().authenticated()                    // protect everything else
                 )
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     return http.build();
     }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+    
 }
