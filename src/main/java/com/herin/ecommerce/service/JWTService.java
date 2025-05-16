943da7ab2245746e03d3024e754fa474ac233d5d
Herin Patel <herin.p45@gmail.com>
2025-05-16 17:52:52 -0400

    Modify JWTService for claim extraction, and validation using JJWT 0.12.6

--- src/main/java/com/herin/ecommerce/service/JWTService.java
+++ src/main/java/com/herin/ecommerce/service/JWTService.java
@@ -13,6 +13,7 @@ import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JWTService {
@@ -43,10 +44,31 @@ public class JWTService {
 
 
     public String extractUserName(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+    
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     public boolean validateToken(String token, UserDetails userDetails) {
-        return true;
+        return extractUserName(token).equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
     }
 }
