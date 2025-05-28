6f730e023bd5e5f5b4a05992b5b0431704d01a8a
Herin Patel <herin.p45@gmail.com>
2025-05-28 12:58:03 -0400

    Add Cart Route in App.jsx with PrivteRoute wrapper

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,12 +9,24 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
+                {/* Define your routes here */}
                 <Route path="/login" element={<Login />} />
+
+                {/* Register route */}
                 <Route path="/register" element={<Register />} />
+
+                {/* Product routes */}
                 <Route path="/products" element={<PrivateRoute>
                     <ProductList />
                 </PrivateRoute>} />
+
                 <Route path="/"  element={<Spinner />} />
+
+                {/* Cart route */}
+                <Routes path="/cart" element={<PrivateRoute>
+                    <Cart />
+                </PrivateRoute>} />
+
             </Routes>
         </BrowserRouter>
     );
