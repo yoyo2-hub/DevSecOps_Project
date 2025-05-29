5df34c0d849f7658ebcc98065aa7ba42f8d4311c
Herin Patel <herin.p45@gmail.com>
2025-05-29 17:12:55 -0400

    Add CartController with GET /cart endpoint using authenticated user

---
+++ backend/src/main/java/com/herin/ecommerce/controller/CartController.java
@@ -0,0 +1,45 @@
+package com.herin.ecommerce.controller;
+
+import com.herin.ecommerce.model.CartItemEntity;
+import com.herin.ecommerce.model.UserEntity;
+import com.herin.ecommerce.model.UserPrincipal;
+import com.herin.ecommerce.service.AuthService;
+import com.herin.ecommerce.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/cart")
+public class CartController {
+
+    private final CartService cartService;
+
+    /**
+     * Constructor for CartController
+     * 
+     * @param cartService CartService instance
+     */
+    @Autowired
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    /**
+     * Get all cart items for a user
+     * 
+     * @param userPrincipal UserPrincipal instance of the authenticated user
+     * @return ResponseEntity<List<CartItemEntity>> list of cart items
+     */
+    @GetMapping
+    public ResponseEntity<List<CartItemEntity>> getCartItems(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+            return ResponseEntity.ok(cartService.getCartItemsByUserId(userPrincipal.getUser().getId()));
+    }
+
+}
