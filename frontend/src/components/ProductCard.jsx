8a9c7493b0618fe92fb9db556c2c337b56ad6f6d
Herin Patel <herin.p45@gmail.com>
2025-06-07 12:13:25 -0400

    Add a + and - css styling for ProductCard

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -14,9 +14,22 @@ function ProductCard({ img, description, name, price }) {
                     <p className="text-sm font-medium text-gray-500 dark:text-gray-400">${price}</p>
                     <h2 className="text-lg font-bold text-gray-800 dark:text-white mt-1">{name}</h2>
                     <p className="mt-2 text-sm text-gray-600 dark:text-gray-300 line-clamp-3">{description}</p>
-                    <button className="mt-6 w-full rounded-lg bg-blue-600 px-4 py-2 text-white text-sm font-semibold transition hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-400">
-                        Add to Cart
-                    </button>
+                    {/*<button className="mt-6 w-full rounded-lg bg-blue-600 px-4 py-2 text-white text-sm font-semibold transition hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-400">*/}
+                    {/*    Add to Cart*/}
+                    {/*</button>*/}
+                    <div className="flex items-center justify-center mt-4 text-gray-800 dark:text-white">
+                        <button
+                            className="px-3 py-1 bg-red-600  font-semibold rounded-l transition hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-red-400">
+                            −
+                        </button>
+                        <span className="px-4 py-1">1</span>
+                        <button
+                            className="px-3 py-1 bg-blue-600  font-semibold rounded-r transition hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-400 ">
+                            +
+                        </button>
+                    </div>
+
+
                 </div>
             </div>
         </Link>
