528975fcc19b98b4af384ae10a92acd83c262d07
Herin Patel <herin.p45@gmail.com>
2025-06-12 14:11:26 -0400

    Add CrossOrigin annotation to ProductController for improved CORS support

--- backend/src/main/java/com/herin/ecommerce/controller/ProductController.java
+++ backend/src/main/java/com/herin/ecommerce/controller/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
