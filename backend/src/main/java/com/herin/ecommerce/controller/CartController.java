103748f60f44c7b451fd005d99fa1a2085ec3aed
Herin Patel <herin.p45@gmail.com>
2025-06-23 13:01:48 -0400

    Add CrossOrigin annotation to CartController for CORS support

--- backend/src/main/java/com/herin/ecommerce/controller/CartController.java
+++ backend/src/main/java/com/herin/ecommerce/controller/CartController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/v1/cart")
 public class CartController {
