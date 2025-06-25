fba7c6bc23f430dc229220d6b0fd3886d924da71
Herin Patel <herin.p45@gmail.com>
2025-06-25 13:41:19 -0400

    Wrap App component with CartProvider to integrate cart context management

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {AuthProvider} from "./context/AuthContext";
+import {CartProvider} from "./context/CartContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <AuthProvider>
-        <App />
+          <CartProvider>
+              <App />
+          </CartProvider>
       </AuthProvider>
   </React.StrictMode>
 );
