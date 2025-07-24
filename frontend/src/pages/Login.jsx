909735099cc7e882e8fcf1b8f3cfdd3fa0bf5eb7
Herin Patel <herin.p45@gmail.com>
2025-07-24 14:29:56 -0400

    Test update heading for deploy test

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -76,7 +76,7 @@ function Login() {
             <div className="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg dark:bg-gray-800 shadow-md">
                 <div className="px-6 py-4">
                     <h3 className="mt-3 text-xl font-medium text-center text-gray-600 dark:text-gray-200">
-                        <span className="text-blue-600 dark:text-blue-500">Welcome </span>
+                        <span className="text-blue-600 dark:text-blue-500">Hi </span>
                         Back
                     </h3>
                     <p className="mt-1 text-center text-gray-500 dark:text-gray-400">
