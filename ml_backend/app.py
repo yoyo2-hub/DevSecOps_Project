804d9a979e9ec569c9762ba839830063206121d8
Herin Patel <herin.p45@gmail.com>
2025-08-06 13:10:50 -0400

    Refactor project structure by moving upload and model modules to the root directory and updating import paths

--- ml_backend/app.py
+++ ml_backend/app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 
-from script.src.upload import upload_bp
+from upload import upload_bp
 
 app = Flask(__name__)
 # Enable CORS for all routes
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.register_blueprint(upload_bp)
+app.register_blueprint(search_bp, url_prefix='/api/v1')
 
 if __name__ == '__main__':
     app.run(debug=True)
