7dbc7652981a90f23cf944093896beb6632ee606
Herin Patel <herin.p45@gmail.com>
2025-08-08 15:08:25 -0400

    Refactor index building process; streamline model and transform retrieval, and improve FAISS index creation

--- ml_backend/bulid_index.py
+++ ml_backend/bulid_index.py
@@ -4,18 +4,18 @@ import pickle
 import torch
 import numpy as np
 from PIL import Image
-from model import model, transform
 import os
+from model import get_model_and_transform
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # path to current file
 product_dir = os.path.join(BASE_DIR, 'static')
-
-
 
 image_paths = [os.path.join(product_dir, f) for f in os.listdir(product_dir) if f.endswith('.jpg')]
 
 vectors = []
 ids = []
+
+model, transform = get_model_and_transform()  # Get the model and transform once
 
 for path in image_paths:
     img = Image.open(path).convert("RGB")
@@ -25,17 +25,15 @@ for path in image_paths:
     vectors.append(vec)
     ids.append(path)
 
-# Convert to float32 (required by FAISS)
 vectors_np = np.vstack(vectors).astype('float32')
 
-# Build FAISS index
-index = faiss.IndexFlatL2(2048)
+index = faiss.IndexFlatL2(vectors_np.shape[1])  # dimension from data
 index.add(vectors_np)
 
-# Save index and ID map
 os.makedirs("embeddings", exist_ok=True)
 faiss.write_index(index, "embeddings/index.faiss")
+
 with open("embeddings/ids.pkl", "wb") as f:
     pickle.dump(ids, f)
 
-
+print("✅ FAISS index built and saved.")
