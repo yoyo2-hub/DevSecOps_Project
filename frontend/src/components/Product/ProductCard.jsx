c81571c12f3d270514b0d2c78117eb424ed619ff
Herin Patel <herin.p45@gmail.com>
2025-08-23 16:54:29 -0400

    Adjust spacing in ProductCard component for improved layout

--- frontend/src/components/Product/ProductCard.jsx
+++ frontend/src/components/Product/ProductCard.jsx
@@ -36,7 +36,7 @@ function ProductCard({ id, img, description, name, price }) {
                 </p>
 
                 {/* pushes button to bottom */}
-                <div className="mt-auto">
+                <div className="mt-4 mb-4">
                     <AddToCartControls id={id} />
                 </div>
             </div>
